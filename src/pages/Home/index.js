28c1140fa08a3c5e93ccbceeec97c844d1c1cbb8
Paulo451 <pauloszuparits@hotmail.com>
2022-04-05 15:27:32 -0300

    was added a button to scroll down the page on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,27 @@
 import Header from "../../components/Header"
 import videoH from '../../assets/video-home-2.mp4'
 import './home.css'
+import { useRef } from "react"
 
 export default function Home(){
+  
+    const ref = useRef(null);
+    const executeScroll = () => ref.current.scrollIntoView();
+
     return(
         <div >
             <Header/>
             <div className="home-1">
                 <div className="titulo-home">
                     <h1>Projeto portifolio </h1> 
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem consequuntur, officia magnam quaerat quasi vitae et ipsum animi illo delectus eum, vero voluptatem nobis labore quos earum sint recusandae deleniti?</p>
+                    <button onClick={executeScroll}>Saiba mais sobre este projeto</button>
                 </div>
                 <video width="100%" autoPlay muted loop>
                     <source src={videoH} />
                     
                 </video>
                 
+                <div className="texto-home-1" ref={ref}><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis cupiditate ducimus quas quibusdam maiores laboriosam eius. Fugit voluptates aperiam quo neque numquam obcaecati delectus facilis sit cum! Accusamus, minus culpa?</p></div>
             </div>
         </div>
     )
